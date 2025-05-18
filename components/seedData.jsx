09749cb6711a07b5358807c7058d86c064311749
Chronic-W-F <vandergriff4004@comcast.net>
2025-05-18 04:45:42 -0500

    Update seedData.jsx

--- components/seedData.jsx
+++ components/seedData.jsx
@@ -6,4 +6,19 @@ export const BudIcon = () => (
   <span style={{ marginRight: '0.5em', verticalAlign: 'middle' }}>🌿</span>
 );
 
-export const seedData = []; // or your real seed array
+export const seedData = [
+  {
+    slot: 1,
+    breeder: 'CopyCat Genetics',
+    strain: 'Hell Runtz',
+    sex: 'Feminized',
+    type: 'Photoperiod'
+  },
+  {
+    slot: 2,
+    breeder: 'THC',
+    strain: 'Vampire Kush',
+    sex: 'Feminized',
+    type: 'Photoperiod'
+  }
+];
