9b91d750b4db47e8285c0665a28cc5bf05069858
Chronic-W-F <vandergriff4004@comcast.net>
2025-05-17 19:49:06 -0500

    Update seedlist.js

--- api/seedlist.js
+++ api/seedlist.js
@@ -1,11 +1,13 @@
 module.exports = async function handler(req, res) {
   try {
-    const response = await fetch(
-      "https://docs.google.com/document/d/1FSxo3B5Sw4oNX8eD6akMLmLQi_8gL7quxWvT4k8-dlk/export?format=txt"
-    );
-    if (!response.ok) {
-      throw new Error("Failed to fetch document");
+    const docUrl = req.query.url;
+    if (!docUrl) {
+      return res.status(400).send("Missing URL");
     }
+
+    const response = await fetch(docUrl);
+    if (!response.ok) throw new Error("Failed to fetch document");
+
     const text = await response.text();
     res.status(200).send(text);
   } catch (err) {
