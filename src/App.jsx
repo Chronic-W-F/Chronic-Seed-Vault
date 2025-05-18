670257d5e275348def0de9ecdecab93005c42a39
Chronic-W-F <vandergriff4004@comcast.net>
2025-05-18 04:39:48 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { DNAIcon, BudIcon } from '../components/seedData';
+import { DNAIcon, BudIcon } from '../components/seedData.jsx';
 
 // --- Alias Mapping ---
 const breederAliases = {
