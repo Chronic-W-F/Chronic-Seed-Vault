e7563996d27a444fe8c95792388fb183305e63cc
Chronic-W-F <vandergriff4004@comcast.net>
2025-05-18 04:27:38 -0500

    Update seedData.js

--- components/seedData.js
+++ components/seedData.js
@@ -1,11 +1,10 @@
-export const dnaIcon = (
-  <svg viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em">
-    <path d="M13 2v2h-2v2h2v2h-2v2h2v2h-2v2h2v2h-2v2h2v2h-2v2h-2v-2H7v-2h2v-2H7v-2h2v-2H7v-2h2V8H7V6h2V4H7V2h2V0h2v2h2z" />
-  </svg>
+export const DNAIcon = () => (
+  <span style={{ marginRight: '0.5em', verticalAlign: 'middle' }}>🧬</span>
 );
 
-export const budIcon = (
-  <svg viewBox="0 0 24 24" fill="currentColor" width="1em" height="1em">
-    <path d="M12 2C10 7 8 9 4 10c3 2 5 5 2 8 4-1 6-1 8 2 0-4 0-6 2-8-4-1-6-3-4-10z" />
-  </svg>
+export const BudIcon = () => (
+  <span style={{ marginRight: '0.5em', verticalAlign: 'middle' }}>🌿</span>
 );
+
+// Export dummy seedData for now if needed
+export const seedData = [];
